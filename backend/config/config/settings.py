4f96e537bd92952bee7d5515c841746d4532e66b
Enmanuel Bernal <63366434+emabercust@users.noreply.github.com>
2026-03-29 13:37:54 -0300

    Update settings.py

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -83,7 +83,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
     #'default': {
          
        # 'ENGINE': 'django.db.backends.postgresql',

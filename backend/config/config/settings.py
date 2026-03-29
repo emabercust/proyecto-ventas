87853605f008158fdb048f62c3daf07275f9c14b
Enmanuel Bernal <63366434+emabercust@users.noreply.github.com>
2026-03-29 13:10:21 -0300

    Update settings.py

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': dj_database_url.config()
     #'default': {
          
        # 'ENGINE': 'django.db.backends.postgresql',

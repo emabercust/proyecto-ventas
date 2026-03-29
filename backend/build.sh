63248cb3bb6d6008c14ff93cdc9fcc0e986bd77a
Enmanuel Bernal <63366434+emabercust@users.noreply.github.com>
2026-03-29 12:52:35 -0300

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -5,3 +5,4 @@ cd config
 
 python manage.py migrate
 python manage.py collectstatic --noinput
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')" | python manage.py shell

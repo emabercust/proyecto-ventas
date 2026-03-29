ba12b327018775b309c754109a3e77d82bd0f0fa
Enmanuel Bernal <63366434+emabercust@users.noreply.github.com>
2026-03-29 13:52:07 -0300

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
-pip install -r requirements.txt
 
-cd config
+echo "📁 Entrando a backend/config..."
+cd backend/config || exit
 
+echo "📦 Instalando dependencias..."
+pip install -r ../requirements.txt
+
+echo "🧱 Migrando base de datos..."
 python manage.py migrate --noinput
+
+echo "📁 Collect static..."
 python manage.py collectstatic --noinput
+
+echo "👤 Creando superusuario..."
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')" | python manage.py shell

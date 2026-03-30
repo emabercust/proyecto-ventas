66e26d2a4728ed56252077f81268ec908e8f0820
Enmanuel Bernal <63366434+emabercust@users.noreply.github.com>
2026-03-30 11:46:56 -0300

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -9,17 +9,21 @@ pip install -r ../requirements.txt
 echo "🧱 Migrando base de datos..."
 python manage.py migrate --noinput
 
-echo "Creando superusuario..."
+echo "Creando superusuario (si no existe)..."
 python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@gmail.com", "admin123")
+username = "admin"
+email = "admin@gmail.com"
+password = "admin123"
+
+if not User.objects.filter(username=username).exists():
+    print("Creando superusuario...")
+    User.objects.create_superuser(username, email, password)
+else:
+    print("El superusuario ya existe")
 END
 
 echo "📁 Collect static..."
 python manage.py collectstatic --noinput
-
-echo "👤 Creando superusuario..."
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')" | python manage.py shell

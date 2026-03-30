33af64670aaf4b076755371260a7e48f24ab25ce
Enmanuel Bernal <63366434+emabercust@users.noreply.github.com>
2026-03-30 16:05:05 -0300

    Update AdminRoute.js

--- frontend/src/routes/AdminRoute.js
+++ frontend/src/routes/AdminRoute.js
@@ -1,16 +1,24 @@
 import { Navigate } from "react-router-dom";
+import {useAuth} from "../context/AuthContext";
 
 //children = la página que se quiere proteger.
 const AdminRoute = ({ children }) => {
-  //Busca en el navegador si existe el token guardado. Recordar que el login guarda esto:
-  //localStorage.setItem('token', response.data.access_token);
-  const token = localStorage.getItem("token");
-  const role = localStorage.getItem("role");
+  const {user} = useAuth();
 
-  if (!token || role !== "admin") {
-    return <Navigate to="/" />;
+  //mientras carga
+  if (user === null) return null;
+
+  //no logeado
+  if (!user) {
+    resturn <Navigate to="/admin/login" />
   }
 
+  //no es admin
+  if (user.role !== "admin") {
+    return <Navigate to="/" />;
+  }
+  
+  //es admin
   return children;
 };
 

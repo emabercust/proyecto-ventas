0a91901b5734e4e3dfb2f02e6afa4c9161564d9a
Enmanuel Bernal <63366434+emabercust@users.noreply.github.com>
2026-03-30 11:26:45 -0300

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -9,6 +9,15 @@ pip install -r ../requirements.txt
 echo "🧱 Migrando base de datos..."
 python manage.py migrate --noinput
 
+echo "Creando superusuario..."
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@gmail.com", "admin123")
+END
+
 echo "📁 Collect static..."
 python manage.py collectstatic --noinput
 
